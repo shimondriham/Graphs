6e446679f1d5442268bb60331521b7b96e7f2177
shimon driham <95618903+shimondriham@users.noreply.github.com>
2022-11-01 16:04:41 +0200

    test_2

--- src/comps/home.js
+++ src/comps/home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home() {
     return (
-        <div style={{ height: 620 }}>
-            Home
+        <div className='text-center' style={{ height: 620 }}>
+            <h1>graph of samtungs : </h1>
         </div>
     )
 }
